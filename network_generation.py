c553c30ccf6a2745501b64f16938436c99644659
Onur Tuncay Bal <94485797+welkiej7@users.noreply.github.com>
2024-10-23 16:02:12 +0200

    Add files via upload

---
+++ network_generation.py
@@ -0,0 +1,220 @@
+import igraph as ig
+import numpy as np
+from tqdm import tqdm
+from copy import deepcopy
+
+'''
+Generation of the follower and following network and the retweet network as
+a half agent based model.
+'''
+
+def calculate_similarity(user_space:np.array, normalize = True):
+
+    '''
+    Calculates the user similarity with euclidean distance and normalizes it
+    between 0 and 1.
+    '''
+
+    user_similarity_matrix = np.zeros(shape=(user_space.shape[0],user_space.shape[0]))
+    for user in range(user_space.shape[0]):
+        for connection in range(user, user_space.shape[0]):
+            user_similarity_matrix[user,connection] = np.linalg.norm(user_space[user] - user_space[connection])
+
+    user_similarity_matrix = user_similarity_matrix + user_similarity_matrix.T - np.diag(user_similarity_matrix.diagonal())
+
+    #normalize
+    if normalize:
+        user_similarity_matrix = np.max(user_similarity_matrix) - user_similarity_matrix
+
+        user_similarity_matrix = (user_similarity_matrix - np.min(user_similarity_matrix))/(np.max(user_similarity_matrix) - np.min(user_similarity_matrix))
+
+        return user_similarity_matrix
+    else:
+        return user_similarity_matrix
+
+
+
+def generate_follower_network_directed_vanilla(deg_dist:list,
+                              homophily_level:int,
+                              user_similarity_matrix:np.array,
+                              prune = True) -> ig.Graph:
+
+    '''
+    No self loops sequential vote casting. Multiple edges are permitted.
+
+    '''
+
+
+
+    if user_similarity_matrix.shape[0] != len(deg_dist):
+        raise ValueError('Length mismatch.')
+
+    edge_list = []
+    for node in range(len(deg_dist)):
+        similarity = user_similarity_matrix[:,node]
+        similarity[node] = 0
+        similarity = (similarity ** homophily_level) / np.sum(similarity ** homophily_level)
+        tot_vot_right = int(deg_dist[node].item())
+        for __ in range(tot_vot_right):
+            possible_cast = np.random.choice(range(len(deg_dist)), size = 1, p = similarity).item()
+            print('\x1b[0;30;47m' +f'Suggesting {node} ->> {possible_cast}'+ '\x1b[0m')
+            print('\x1b[0;30;46m' +f'Accepted {node} ->> {possible_cast}'+ '\x1b[0m')
+            edge_list.append((node,possible_cast))
+    if prune:
+        return ig.Graph(n = user_similarity_matrix.shape[0], edges = edge_list, directed = True).simplify(multiple = True)
+    else:
+        return ig.Graph(n = user_similarity_matrix.shape[0], edges = edge_list, directed = True)
+
+
+
+
+
+def generate_follower_network_exhaustive(out_deg_dist:list,
+                                        in_deg_dist:list,
+                                        homophily_level:int,
+                                        user_similarity_matrix:np.array,
+                                        generation_tolerance = 1e4):
+
+
+    '''
+    No self loops, sequential vote casting. Multiple edges are not allowed
+    and degree distribution is absolute.
+    '''
+
+    if (user_similarity_matrix.shape[0] != len(out_deg_dist)) or (user_similarity_matrix.shape[0] != len(out_deg_dist)):
+        raise ValueError('Length mismatch')
+
+    edge_list = [] #Initialize the edge list.
+
+    # For each node suggest a cast to a possible node.
+    # If possible node connection does not exist and offered node has acceptance for the indegree, accept the offer.
+
+    for NODE in range(len(out_deg_dist)):
+        similarity_list = user_similarity_matrix[:,NODE]
+        similarity_list[NODE] = 0 # Prevent self loops
+        similarity_list = (similarity_list**homophily_level) / (np.sum(similarity_list**homophily_level))
+        casting_right = out_deg_dist[NODE]
+
+        for CAST in range(int(casting_right.item())):
+            CONDITION = False
+            attempt = 0
+            while not CONDITION:
+                POSSIBLE_CAST = np.random.choice(range(user_similarity_matrix.shape[0]), size = 1, p = similarity_list).item()
+                print('\x1b[0;30;47m' +f'Suggesting {NODE} ->> {POSSIBLE_CAST}'+ '\x1b[0m')
+                if (NODE,POSSIBLE_CAST) in edge_list:
+                    print('\x1b[0;37;41m' +f'Rejected: Multiple {NODE} ->> {POSSIBLE_CAST}'+ '\x1b[0m')
+                    similarity_list[POSSIBLE_CAST] = 0
+                    similarity_list = (similarity_list) / (np.sum(similarity_list))
+                elif in_deg_dist[POSSIBLE_CAST] < 1:
+                    print('\x1b[0;37;41m' +f'Rejected: No Right {NODE} ->> {POSSIBLE_CAST}'+ '\x1b[0m')
+                    similarity_list[POSSIBLE_CAST] = 0
+                    similarity_list = (similarity_list) / (np.sum(similarity_list))
+
+                else:
+                    print('\x1b[0;30;46m' +f'Accepted {NODE} ->> {POSSIBLE_CAST}'+ '\x1b[0m')
+                    edge_list.append((NODE,POSSIBLE_CAST))
+                    in_deg_dist[POSSIBLE_CAST] = in_deg_dist[POSSIBLE_CAST] - 1
+                    CONDITION = True
+
+                if attempt == generation_tolerance:
+                    raise TimeoutError('Could not generate a network within the given tolerance')
+
+                attempt += 1
+
+    return ig.Graph(n = user_similarity_matrix.shape[0], edges = edge_list, directed = True)
+
+
+def generate_retweet_network(follower_following_network:ig.Graph,
+                             number_of_retweets:list,
+                             echo_chamber_strength:float,
+                             homophily_level:int,
+                             user_space:np.array,
+                             topic:int,
+                             single_retweet:bool = True):
+    similarity_matrix = calculate_similarity(user_space[:,topic], normalize= True)
+
+
+
+    rt_list = []
+    for NODE in tqdm(range(len(number_of_retweets)), desc = 'Retweeting'):
+        try:
+            RT_RIGHT = number_of_retweets[NODE]
+            similarity_list = similarity_matrix[:,NODE]
+            similarity_list[NODE] = 0 # No Self Retweets
+            similarity_list = (similarity_list ** homophily_level) / (np.sum(similarity_list**homophily_level))
+
+            for __ in range(RT_RIGHT):
+                CAST_TYPE = bool(np.random.binomial(1, p = echo_chamber_strength))
+
+                if CAST_TYPE:
+                    poss = follower_following_network.neighbors(NODE, mode = 'in')
+                    similarity_list_neigh = similarity_list[poss] / np.sum(similarity_list[poss])
+                    POSSIBLE_CAST = np.random.choice(poss, size = 1,p = similarity_list_neigh).item()
+                    print('\x1b[0;30;45m' + f'Retweet {NODE} ->> {POSSIBLE_CAST}, Echo Chamber' + '\x1b[0m')
+                    
+                else:
+                    POSSIBLE_CAST = np.random.choice(range(similarity_matrix.shape[0]), size = 1, p = similarity_list).item()
+
+                print('\x1b[0;30;44m' + f'Retweet {NODE} ->> {POSSIBLE_CAST}, Feed Observation' + '\x1b[0m')
+
+                rt_list.append((NODE,POSSIBLE_CAST))
+        except Exception as e:
+            print(e)
+            continue
+
+    
+    return ig.Graph(similarity_matrix.shape[0], rt_list, directed = True)
+
+
+    """
+    For each node:
+
+        Get the retweet right
+        Cast n numbers of retweet according to their right and similarity matrix in one topic.
+
+
+
+    """
+
+
+
+if __name__ == "__main__":
+    from user_space_models import generate_user_space
+    from network_generation import calculate_similarity, generate_follower_network_exhaustive, generate_retweet_network
+    import numpy as np
+
+    some_space = generate_user_space(n_users= 100,
+                            n_cleavage= 3,
+                            n_non_cleavage= 1,
+                            n_noise= 1,
+                            n_unifying_topics= 1,
+                            unification_point= 0.4,
+                            cleavage_alignment= 0.95,
+                            non_cleavage_alignment= 0.9,
+                            bimodal_polarization_strength= 2,
+                            non_polarization_attraction=8,
+                            clevage_alignment_noise= 0.1,
+                            non_cleavage_alignment_noise=0.1,
+                            unification_strength=100,
+                            unification_alignment_noise=0.01,
+                            unification_alignment=1,
+                            noise_topic_noise= 0.8)
+
+
+
+
+    similarity_matrix = calculate_similarity(some_space)
+    out_deg_dist = np.random.uniform(low = 1, high= 5, size = some_space.shape[0])
+    in_deg_dist = np.random.uniform(low= 1, high= 5, size = some_space.shape[0])
+    network = generate_follower_network_exhaustive(out_deg_dist,in_deg_dist, 
+                                                   homophily_level= 10, 
+                                                   user_similarity_matrix= similarity_matrix, 
+                                                   generation_tolerance= 1e4)
+    retweet_right = [round(i) for i in np.random.uniform(low = 0, high = 5, size = similarity_matrix.shape[0])]
+    rt = generate_retweet_network(network, 
+                             number_of_retweets=retweet_right,
+                             echo_chamber_strength= 0.8,
+                             similarity_matrix= similarity_matrix,
+                             homophily_level= 5,
+                             single_retweet= True)
+    print(rt)
--- user_space_models.py
+++ user_space_models.py
@@ -6,7 +6,7 @@ import warnings
 def generate_aligned_array(original_array:np.array,
                            alignment_strength:float = 0.8,
                            noise_level:float = 0.2):
-    
+
     if alignment_strength > 1 or alignment_strength < 0:
         raise ValueError(f'Alignment strength is outside of the range (0,1)')
     if noise_level > 1 or noise_level < 0:
@@ -16,17 +16,17 @@ def generate_aligned_array(original_array:np.array,
     aligned_array = alignment_strength * original_array + (1 - alignment_strength) * generated_noise
     aligned_array = np.clip(aligned_array, -1, 1)
 
-    
+
     return aligned_array
 
-def generate_user_space(n_users:int, 
+def generate_user_space(n_users:int,
                         n_cleavage:int,
                         n_non_cleavage:int,
                         n_noise:int,
                         n_unifying_topics:int,
                         unification_point:float,
                         cleavage_alignment:float,
-                        non_cleavage_alignment:int,
+                        non_cleavage_alignment:float,
                         bimodal_polarization_strength:float=2,
                         non_polarization_attraction:float = 8,
                         clevage_alignment_noise:float = 0.2,
@@ -35,22 +35,22 @@ def generate_user_space(n_users:int,
                         unification_alignment_noise:float = 0.01,
                         unification_alignment:int = 1,
                         noise_topic_noise:float = 0.8):
-    
+
     '''
     Definition: User space generator for the network generation model. Contains 7 parameters
     to generate a (n_cleavage + n_non_cleavage + n_noise + n_unifying_topics) * n_users matrix.
     Each user has an opinion in the total generated topics between -1 and 1. Alignment parameters will
-    set how much these opinions are aligned. 
+    set how much these opinions are aligned.
 
 
     @param:int n_users = Number of users to be generated.
     @param:int n_cleavage = Number of cleavage topics to be generated.
-    @param:int n_non_cleavage = Number of non - cleavage topics to be generated. 
+    @param:int n_non_cleavage = Number of non - cleavage topics to be generated.
     @param:int n_noise = Number of noise, or uniform, or irrelevant topics to be generated.
     @param:int n_unifying_topics = Number of unifying topics to be generated
     @param:float unification_point = The point of unification for the generated unifying topics.
     @param:float cleavage_alignment = The alignment for the clevage topics, 1 is perfect alignment, 0 is no alignment at all.
-    @param:float non_cleavage_alignment = The alignment for the non clevage topics, standard deviation for a 
+    @param:float non_cleavage_alignment = The alignment for the non clevage topics, standard deviation for a
     normal distribution for each user's score in a non cleavage topic.
     @param:float bimodal_polarization_strength = The inverse will be passed as the alpha and the beta of the
     beta distribution parameters.
@@ -61,7 +61,7 @@ def generate_user_space(n_users:int,
     #Set the user space where each row is a user and each column is a topic.
     user_space = np.zeros(shape=(n_users,n_cleavage + n_non_cleavage + n_noise + n_unifying_topics))
     #For each topic type we have a different generation strategy. After generating we are going to
-    #assign it to a row in the user space matrix. First generate the polarized topics. 
+    #assign it to a row in the user space matrix. First generate the polarized topics.
     #Check the bimodal_polarization_strength
     if bimodal_polarization_strength < 1:
         warnings.warn('Bimodal Polarization strength is too low to generate a polarized distribution.')
@@ -74,7 +74,7 @@ def generate_user_space(n_users:int,
     for COL in range(n_cleavage):
         TMP_aligned_array = generate_aligned_array(core_polarized, alignment_strength= cleavage_alignment, noise_level= clevage_alignment_noise)
         user_space[:,COL] = TMP_aligned_array
-    del TMP_aligned_array, core_polarized # No point to keep them in memory.
+
     if non_polarization_attraction < 1e-2:
         warnings.warn('Non polarization attraction is too low to generate a non polarized distribution.')
     non_polarization_attraction = 1 / non_polarization_attraction
@@ -82,18 +82,18 @@ def generate_user_space(n_users:int,
     for COL in range(n_cleavage, n_cleavage + n_non_cleavage):
         TMP_aligned_array = generate_aligned_array(core_non_polarized, alignment_strength= non_cleavage_alignment, noise_level= non_cleavage_alignment_noise)
         user_space[:,COL] = TMP_aligned_array
-    del TMP_aligned_array, core_non_polarized # No point to keep them in memory
+
     # Generate noise topics.
     noise_topics = np.random.uniform(-1,1,size = n_users)
     for COL in range(n_cleavage + n_non_cleavage, n_cleavage + n_non_cleavage + n_noise):
         user_space[:,COL] = generate_aligned_array(noise_topics, alignment_strength= 0.1, noise_level= noise_topic_noise)
-    del noise_topics
+
     #Generate unified topics.
     unification_strength = 1 / unification_strength
     unified_topics = np.random.normal(loc = unification_point, scale= unification_strength, size = n_users)
     for COL in range(n_cleavage + n_non_cleavage + n_noise, n_cleavage + n_non_cleavage + n_noise + n_unifying_topics):
         user_space[:,COL] = generate_aligned_array(unified_topics, unification_alignment, unification_alignment_noise)
-    del unified_topics
+
 
 
     return user_space
@@ -103,7 +103,7 @@ def generate_user_space(n_users:int,
 if __name__ == '__main__':
 
     generate_user_space(n_users= 100,
-                        n_cleavage=10, 
+                        n_cleavage=10,
                         n_non_cleavage=3,
                         n_noise=2,
                         n_unifying_topics=1,
